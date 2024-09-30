84c866361b1c96df0aeaf86c8b99fc51c9b59d8a
Jack Smith <mr.smithjsk@gmail.com>
2024-09-30 21:32:12 +0100

    Attempt at CORS issue fix

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -15,7 +15,7 @@ router.post('/set-email', (req, res) => {
         console.log('Email set in session:', req.session.userEmail);
         console.log('Email set in session 2:', email);
         console.log('Session ID:', req.sessionID);
-        res.sendStatus(200).json({ email: req.session.userEmail });
+        res.sendStatus(200);
     } else {
         res.sendStatus(400);
     }
