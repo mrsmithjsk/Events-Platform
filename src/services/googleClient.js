dfedb9f75dde6876ae077fac2ccd55608a3f0a83
Jack Smith <mr.smithjsk@gmail.com>
2024-10-02 22:12:07 +0100

    Google client change

--- src/services/googleClient.js
+++ src/services/googleClient.js
@@ -1,13 +1,18 @@
 const { google } = require('googleapis');
-const fs = require('fs');
+//const fs = require('fs');
 
-const credentials = JSON.parse(fs.readFileSync('client_secret.json'));
-const { client_secret, client_id, redirect_uris } = credentials.web;
+// const credentials = JSON.parse(fs.readFileSync('client_secret.json'));
+// const { client_secret, client_id, redirect_uris } = credentials.web;
+
+// const oAuth2Client = new google.auth.OAuth2(
+//   client_id, client_secret, redirect_uris[0]
+// );
 
 const oAuth2Client = new google.auth.OAuth2(
-  client_id, client_secret, redirect_uris[0]
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  process.env.REDIRECT_URI
 );
-
 
 module.exports = {
   oAuth2Client
